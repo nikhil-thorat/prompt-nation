a5999a93e2e39f6d1a4bd8cfabc682d8f095a3ba
Nikhil Thorat <nikhilthoratofficial@gmail.com>
2024-09-23 20:37:03 +0530

    setup refactor

--- app/api/chatbot/route.js
+++ app/api/chatbot/route.js
@@ -1,6 +1,6 @@
 import OpenAI from "openai";
 
-const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY || "" });
 
 export const POST = async (req, res) => {
 	const { prompt } = await req.json();
